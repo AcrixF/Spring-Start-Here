726b6922330182fa2567277eefafb6ebe7a405e4
Cristian Flores <acrixf@gmail.com>
2022-03-12 23:48:03 -0600

    sshl-ch02 - Using @Bean annotation to add beans into Spring Context

---
+++ ch02/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import model.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Koko");
+        return parrot;
+    }
+
+    @Bean
+    public String hello() {
+        return "Hello";
+    }
+
+    @Bean
+    public Integer ten() {
+        return 10;
+    }
+
+}
--- ch02/src/test/java/context/ContextTest.java
+++ ch02/src/test/java/context/ContextTest.java
@@ -1,5 +1,7 @@
 package context;
 
+import config.ProjectConfig;
+import model.Parrot;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,10 +10,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ContextTest {
 
     @Test
-    public void springContextIsAdded() {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
-        assertThat(annotationConfigApplicationContext).isNotNull();
+    public void springContextIsInitializedCorrectly() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        assertThat(context).isNotNull();
     }
 
+    @Test
+    public void initializeSpringContextWithConfigurationClassAndReturnsExpectedParrotBean() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot parrot = context.getBean("parrot", Parrot.class);
+        assertThat(parrot).isNotNull()
+                .isExactlyInstanceOf(Parrot.class);
+        assertThat(parrot.getName()).isEqualTo("Koko");
+    }
+
+    @Test
+    public void initializeSpringContextReturnsDefinedBeansInConfigurationFile() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot parrot = context.getBean("parrot", Parrot.class);
+        String hello = context.getBean("hello", String.class);
+        Integer ten = context.getBean("ten", Integer.class);
+
+        assertThat(parrot).isNotNull()
+                .isExactlyInstanceOf(Parrot.class)
+                .hasFieldOrPropertyWithValue("name", "Koko");
+
+        assertThat(hello).isNotNull()
+                .isNotEmpty()
+                .isEqualTo("Hello")
+                .isInstanceOf(String.class);
+
+        assertThat(ten).isNotNull()
+                .isNotZero()
+                .isEqualTo(10)
+                .isInstanceOf(Integer.class);
+    }
 
 }
