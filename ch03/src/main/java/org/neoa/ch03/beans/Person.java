a08f38a0375d3e5af9d985e230844405c3dec85b
Cristian Flores <acrixf@gmail.com>
2022-03-20 00:07:28 -0600

    sshl-ch03-3.4.3 - Injection dependency using Qualifier at constructor level when more than one instance type exists in the spring context

--- ch03/src/main/java/org/neoa/ch03/beans/Person.java
+++ ch03/src/main/java/org/neoa/ch03/beans/Person.java
@@ -3,6 +3,7 @@ package org.neoa.ch03.beans;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -14,7 +15,7 @@ public class Person {
     private final Parrot parrot;
 
     @Autowired
-    public Person(Parrot miki) {
-        this.parrot = miki;
+    public Person(@Qualifier("miki") Parrot parrot) {
+        this.parrot = parrot;
     }
 }
