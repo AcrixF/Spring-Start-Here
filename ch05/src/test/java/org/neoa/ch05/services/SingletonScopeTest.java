43663c572a2145f6fbef5b08dac61152008d14f3
Cristian Flores <acrixf@gmail.com>
2022-03-22 22:51:38 -0600

    sshl-ch05-5.1.2 - Declaring Singleton beans using stereotypes annotations

--- ch05/src/test/java/org/neoa/ch05/services/SingletonScopeTest.java
+++ ch05/src/test/java/org/neoa/ch05/services/SingletonScopeTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
